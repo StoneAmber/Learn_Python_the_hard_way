373cdaf4cb1eb30b364343b00da3ee5dca9bd87a
stonemask <stonemask@users.noreply.github.com>
2017-01-07 14:27:34 +0800

    ex1 add 2 print Chiese chracter methods

--- ex1.py
+++ ex1.py
@@ -8,4 +8,5 @@ print 'Yay! Printing.'
 print "I'd much rather you 'not' " '''
 #print 'I "said" do not touch this.'
 print "All in one line.\nTry these.\tLife is short"
-print "他山之玉 ，可以攻石"
+print u"他山之玉 ，可以攻石"
+print "追远 ".decode('utf-8')
